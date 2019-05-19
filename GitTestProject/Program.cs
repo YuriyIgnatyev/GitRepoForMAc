3de42695016203a03d0d2511748b28ca37e57e93
macbook <yualawyerwork@outlook.com>
2019-05-19 20:44:44 +0300

    Just try to check how it's work.

--- GitTestProject/Program.cs
+++ GitTestProject/Program.cs
@@ -2,11 +2,31 @@
 
 namespace GitTestProject
 {
-    class MainClass
+    public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Git Test Project !");
+            var array = Console.ReadLine().Split(' '); // string
+            var max = 0; // will be rewrited // int.MaxValue; because it dosn't work normally
+            var min = 0; // will be rewrited // int.MinValue; because it dosn't work normally
+            // var С = int.Parse(array[0]); because we dont use it
+            int number;
+            int index; // = 0; because we dont use it
+
+            for (index = 0; index < array.Length; index++) // index = index +1) int index = new index []
+            {
+                number = int.Parse(array[index]); // Создание одномерного массива: целых чисел, необьявленного размера
+                //.Parse covert String from [index (arrey)] to int
+                if (max < number)
+                {
+                    max = number;
+                }
+                if (min > number)
+                {
+                    min = number;
+                }
+            }
+            Console.WriteLine("Minimum = " + min + "Maximum = " + max);
         }
     }
 }
