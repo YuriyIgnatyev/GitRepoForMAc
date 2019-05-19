0ce1fa0703bac0f36ee8b39cec8df80913711a53
YuriyIgnatyev <yualawyer@gmail.com>
2019-05-19 15:29:38 +0300

    Initial check-in of module GitTestProject

---
+++ GitTestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTestProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Git Test Project !");
+        }
+    }
+}
